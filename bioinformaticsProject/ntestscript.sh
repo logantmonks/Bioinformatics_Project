74ce45bcc3bd2c6295cb05b2b6cd7566dba82e87
Natalie Fernandez <nfernan4@crcfe01.crc.nd.edu>
2022-10-12 18:26:40 -0400

    hsp70 script up to seaching 1 proteome

--- bioinformaticsProject/ntestscript.sh
+++ bioinformaticsProject/ntestscript.sh
@@ -1,3 +1,4 @@
+
 #Compiling hsp70 ref sequences
 
 for file in ~/Private/Biocomputing2022/Bioinformatics_Project/bioinformaticsProject/ref_sequences/$hsp70gene_*.fasta
@@ -7,3 +8,10 @@ done
 
 #Using muscle on hsp70 ref sequences
 ~/Private/Biocomputing2022/Tools/muscle -in hsp70_compiled.fasta -out hsp70_genes.afa
+
+#Hmmr Build hsp70
+~/Private/Biocomputing2022/Tools/hmmbuild hsp70_gene_profile.hmmfile hsp70_genes.afa
+
+#Hmmr Search hsp70
+~/Private/Biocomputing2022/Tools/hmmsearch --tblout hsp70_gene_search.hmmfile hsp70_gene_profile.hmmfile ./proteomes/proteome_01.fasta
+
