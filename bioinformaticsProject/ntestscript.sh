be72c2a252814a52ff84aed9df8c2cca56618b43
Natalie Fernandez <nfernan4@crcfe01.crc.nd.edu>
2022-10-14 12:54:40 -0400

    Check ntestscript, will have new format for the script

--- bioinformaticsProject/ntestscript.sh
+++ bioinformaticsProject/ntestscript.sh
@@ -1,3 +1,23 @@
+#Compiling mcrA ref sequecnes
+for file in ~/Private/Biocomputing2022/Bioinformatics_Project/bioinformaticsProject/ref_sequences/mcrAgene_*.fasta
+do
+        cat $file >> mcrA_compiled.fasta
+done
+
+#Using muscle in mcrA ref sequences
+~/Private/Biocomputing2022/Tools/muscle -in mcrA_compiled.fasta -out mcrA_genes.afa
+
+#Hhmbuild mcrA
+~/Private/Biocomputing2022/Tools/hmmbuild mcrA_gene_profile.txt mcrA_genes.afa
+
+#Hhmsearch mcrA
+mcrACount=$(cat mcrA_gene_search.txt | grep WP_ | wc -l)
+#for seq in ~/Private/Biocomputing2022/Bioinformatics_Project/bioinformaticsProject/proteomes/proteome_*.fasta
+#do
+#       ~/Private/Biocomputing2022/Tools/hmmsearch --tblout mcrA_gene_search.txt mcrA_gene_profile.txt ./proteomes/$seq
+#	cat mcrA_gene_search.txt | grep WP_ | wc -l
+
+
 
 #Compiling hsp70 ref sequences
 
@@ -10,12 +30,13 @@ done
 ~/Private/Biocomputing2022/Tools/muscle -in hsp70_compiled.fasta -out hsp70_genes.afa
 
 #Hmmr Build hsp70
-~/Private/Biocomputing2022/Tools/hmmbuild hsp70_gene_profile.hmmfile hsp70_genes.afa
+~/Private/Biocomputing2022/Tools/hmmbuild hsp70_gene_profile.txt hsp70_genes.afa
 
-#Hmmr Search hsp70
-#for file in ~/Private/Biocomputing2022/Bioinformatics_Project/bioinformaticsProject/proteomes/$proteome_*.fasta
+#Hmmr Search hsp70 with variable
+hsp70count=$(cat hsp70_gene_search.txt | grep WP_ | wc -l)
+#for file in ~/Private/Biocomputing2022/Bioinformatics_Project/bioinformaticsProject/proteomes/proteome_*.fasta
 #do
-#	~/Private/Biocomputing2022/Tools/hmmsearch --tblout hsp70_gene_search.txt hsp70_gene_profile.hmmfile ./proteomes/$file
-#	cat hsp70_gene_search.txt | grep "WP_"-c |
+#	~/Private/Biocomputing2022/Tools/hmmsearch --tblout hsp70_gene_search.txt hsp70_gene_profile.txt ./proteomes/$file
+#	cat hsp70_gene_search.txt | grep WP_ | wc -l
 
-~/Private/Biocomputing2022/Tools/hmmsearch --tblout hsp70_gene_search.txt hsp70_gene_profile.hmmfile ./proteomes/proteome_01.fasta
+~/Private/Biocomputing2022/Tools/hmmsearch --tblout hsp70_gene_search.txt hsp70_gene_profile.txt ./proteomes/proteome_01.fasta
--- bioinformaticsProject/ref_sequences/mcrACompiler.sh
+++
@@ -1,7 +0,0 @@
-#Compiles mcrA ref seqs to a single fasta
-
-for file in $mcrA_gene*.fasta
-do
-	cat $file >> mcrA_compiled.fasta
-done
-
