8179fc4c52f77d0e86ecc829316b9c5645b2afb0
Joseph Harris <jharri28@crcfe01.crc.nd.edu>
2022-10-14 14:52:48 -0400

    DONE

--- bioinformaticsProject/HARRIS_JOSEPH_PROJECT.sh
+++ bioinformaticsProject/HARRIS_JOSEPH_PROJECT.sh
@@ -1,9 +1,17 @@
+# HARRIS_JOSEPH_PROJECT.sh is a script designed to allign refrence genomes with a desired gene,
+# model those genomes, and then use that model to search proteomes for desired genes.
+# The script will output a table with each proteome and the number of matches the proteome has for each gene.
+# The script will also output a list of the best candidate proteomes ranked by the prevelence of the deisred genes.
 
-#usage: $bash Test.sh $1 $2
+# usage: $bash HARRIS_JOSEPH_PROJECT.sh $1 $2
+# $1= desired gene 1 (more prevelent)
+# $2= desired gene 2
+# in our case, $1 and $2 would be "hsp70" and "mcrA" respectively
+# This is done make the script more flexible and applicable to different genes
 
-#$1= gene 1 $2= gene 2 (in our case, hsp70 and McrA)
+#assume script is run in "bioinformaticsProject" directory
 
-#assume script is run in ______ dirrectory
+
 
 #first you need to combine the refrence sequences into one file per desired gene
 
@@ -11,7 +19,7 @@ cd ref_sequences
 cat $1*.fasta > $1_combined.fasta
 cat $2*.fasta > $2_combined.fasta
 
-#then create a dir to store the intermediary files for each gene in
+#then create a directory to store the intermediary files for each gene, within the main directory
 
 cd ..
 mkdir $1
@@ -19,17 +27,16 @@ mkdir $2
 
 cd ref_sequences
 
-#move the combined files into their respective dir
+#move the combined files into their respective directories
 
 mv $1_combined.fasta ../$1
 mv $2_combined.fasta ../$2
 
-#return back to _____ dir
 
 cd ..
 
-#First enter the gene dir so that created files will be in appropriate place
-#Then run muscle on the combined.fasta files to allign them
+#First enter the gene directory so that created files will be in appropriate place
+#Then run muscle on the combined.fasta files to allign the files
 #Return stdout to new alligned.fasta file
 #Then run hmmbuild to create an HMMmodel for each gene
 
@@ -43,10 +50,11 @@ cd $2
 ~/Private/Biocomputing2022/tools/hmmbuild gene2HMMmodel.fasta $2_alligned.fasta
 cd ..
 
-#currently in bioinformatics dir
-#HHM models are built, need to search proteomes for genes now
+#currently in bioinformatics directory
+#HHM models are built, now search proteomes for genes
 #use hhmsearch to search for desired genes in each proteome
-#Run a for-loop to create an output
+#Run a for-loop to create an output file for each proteome
+#Each output file will be a table showing the number of copies of the gene in the proteome
 
 cd proteomes
 
@@ -60,24 +68,26 @@ do
 ~/Private/Biocomputing2022/tools/hmmsearch --tblout "$proteome"_"$2".txt ../$2/gene2HMMmodel.fasta $proteome
 done
 
-#now all count files will appear in the proteomes dir. Need to move to respective dirs, first need to make dirs
+#now all count files will appear in the proteomes directory
+#First make directories to store these files
+#Then move these files to their respective directories
 cd ..
 mkdir $1counts
 mkdir $2counts
 
-#need to move all count files to their respective dirs
+#Moving files to their respecitve directories
 
 cd proteomes
 mv *$1.txt ../$1counts
 mv *$2.txt ../$2counts
 
-#all count files are in correct dir
-#use for loop to grep search for any line that does not start with "#" then count lines to know how many copies
-#search in .fasta.txt to remove the extra file extentions
-
+#First create a file to store the table data in for this gene
 cd ..
 
 echo "proteome $1" > tablematch$1.txt
+
+#Use a for-loop to echo the name of the proteome and the gene count within the file
+#Place this data into the recently created table file
 
 cd $1counts
 for proteome in *.txt
@@ -85,6 +95,8 @@ do
 var1=`cat $proteome | grep -v "#" | wc -l`
 echo "$proteome, $var1" >> ../tablematch$1.txt
 done
+
+#Reapeat above steps for gene 2
 
 cd ..
 
@@ -99,12 +111,25 @@ done
 
 cd ..
 
+#Combine the two table files by taking the second row (the gene matches) of one file and pasting it into the other file
+#This creates a table with three rows in proteome gene-1-match gene-2-match order
+#Remove the excess file extensions to leave proteome names as original
+#Pipe this final table to a new file
+
 cat tablematch$2.txt | cut -d " " -f 2 | paste -d " " tablematch$1.txt - | sed "s/.fasta_hsp70.txt,/ /" > FINALTABLE.txt
+
+#Read information from the final table
+#Get rid of all lines that contain "0", because these cannot be canidates
+#Sort the file by frequency of genes
+#Pipe this data into a new file
 
 cat FINALTABLE.txt | sed "/0/d" | sort -k2 -r > CANDIDATES.txt
 
-# now need to make table
+#Celebrate because you finished the midterm project at the buzzer
 
 echo "WOOOOOOHOOOOOOO IT WORKED"
+
+#Lines to state the two files created in the terminal once the script is run
+
 echo "File Created: FINALTABLE.txt a table with each proteome and the number of copies of each desired gene"
 echo "File Created: CANDIDATES.txt a file with all potential candidates ranked by the prevelence of the genes"
